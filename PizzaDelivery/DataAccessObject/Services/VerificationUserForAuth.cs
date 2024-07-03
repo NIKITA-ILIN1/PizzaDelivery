50a3b9b8ac27612ac855f3e03f643548907582c3
301k10 <301k10@WIN-9M04LT12L1F>
2024-07-03 21:04:51 +0300

    Изменения с дадатой

---
+++ PizzaDelivery/DataAccessObject/Services/VerificationUserForAuth.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using PizzaDelivery.Entity;
+using System;
+
+namespace PizzaDelivery
+{
+    internal class VerificationUserForAuth
+    {
+        public bool CheckExistUser(User user)
+        {
+            SqlConnection sqlConnection = ConnectorDataBaseMicrosoftSQL.StartConnection().SqlConnection;
+
+            try
+            {
+                sqlConnection.Open();
+
+                string sqlQueryCheckExistUser = "SELECT * FROM Users WHERE login = @login AND password = @password";
+
+                SqlCommand sqlCommandCheckExistUser = new SqlCommand(sqlQueryCheckExistUser, sqlConnection);
+
+                sqlCommandCheckExistUser.Parameters.AddWithValue("@login", user.Login);
+                sqlCommandCheckExistUser.Parameters.AddWithValue("@password", user.Password);
+
+                int count = Convert.ToInt32(sqlCommandCheckExistUser.ExecuteScalar());
+
+                return count == 0;
+            }
+            catch (SqlException e)
+            {
+                throw new Exception("Произошла ошибка при работе с базой данных: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Произошла ошибка: " + e.Message);
+            }
+            finally { sqlConnection.Close(); }
+        }
+    }
+}
---
+++ PizzaDelivery/DataAccessObject/Services/VerificationsUserForRegistr.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Data.SqlClient;
+using PhoneNumbers;
+using PizzaDelivery.Entity;
+using System;
+
+namespace PizzaDelivery.DataAccessObject.Services
+{
+    internal class VerificationsUserForRegistr
+    {
+        SqlConnection sqlConnection = ConnectorDataBaseMicrosoftSQL.StartConnection().SqlConnection;
+
+        public static string CheckEmailUser(string emailUser)
+        {
+
+            if (string.IsNullOrEmpty(emailUser))
+            {
+                throw new Exception("Email is null of empty");
+            }
+
+            if (!emailUser.Contains("@"))
+            {
+                throw new Exception("Email not Contains @");
+            }
+
+            return emailUser;
+        }
+
+        public static string CheckPhoneNumberUser(string phoneNumber)
+        {
+
+            PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.GetInstance();
+
+            if (string.IsNullOrEmpty(phoneNumber))
+            {
+                throw new ArgumentException("Phone number null or empty", nameof(phoneNumber));
+            }
+
+            if (!phoneNumber.StartsWith("+"))
+            {
+                phoneNumber = "+" + phoneNumber;
+            }
+
+            try
+            {
+                PhoneNumber number = phoneNumberUtil.Parse(phoneNumber, null);
+
+                string formattednumber = phoneNumberUtil.Format(number, PhoneNumberFormat.INTERNATIONAL);
+
+                if (!phoneNumberUtil.IsValidNumber(number))
+                {
+                    throw new ArgumentException("Phone number not valid", nameof(phoneNumber));
+                }
+
+                return formattednumber;
+            }
+            catch (NumberParseException e)
+            {
+                throw new ArgumentException("Error parsing phone number: " + e.Message);
+            }
+        }
+
+        public bool CheckExistUser(User user)
+        {
+
+            try
+            {
+                sqlConnection.Open();
+
+                string sqlQueryCheckExistUser = "SELECT * FROM Users WHERE phone_number = @phoneNumber " +
+                                                "OR email = @email OR login = @login";
+
+                SqlCommand sqlCommandCheckExistUser = new SqlCommand(sqlQueryCheckExistUser, sqlConnection);
+
+                sqlCommandCheckExistUser.Parameters.AddWithValue("@phoneNumber", user.PhoneNumber);
+                sqlCommandCheckExistUser.Parameters.AddWithValue("@email", user.Email);
+                sqlCommandCheckExistUser.Parameters.AddWithValue("@login", user.Login);
+
+                int count = Convert.ToInt32(sqlCommandCheckExistUser.ExecuteScalar());
+
+                return count == 0;
+            }
+            catch (SqlException e)
+            {
+                throw new Exception("Произошла ошибка при работе с базой данных: " + e.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Произошла ошибка: " + ex.Message);
+            }
+            finally { sqlConnection.Close(); }
+        }
+    }
+}
