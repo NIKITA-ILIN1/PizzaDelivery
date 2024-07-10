78390d9b0613446d7f13643ff6599f9480a22ec9
301k10 <301k10@WIN-9M04LT12L1F>
2024-07-10 19:48:36 +0300

    testcommet

--- PizzaDelivery/WindowsForms/PersonalAccount.cs
+++ PizzaDelivery/WindowsForms/PersonalAccount.cs
@@ -33,5 +33,15 @@ namespace PizzaDelivery.WindowsForms
 
             Application.Exit();
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
